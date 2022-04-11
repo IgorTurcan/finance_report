73ebe990da90a05a9d001453b03cc211d2f036d1
Igor Turcan <igortsurkan99@gmail.com>
2022-04-11 17:29:54 +0300

    Resize loading

--- lib/add_expense/widgets/loading_wheel.dart
+++ lib/add_expense/widgets/loading_wheel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:loading_indicator/loading_indicator.dart';
 
 import '../../constants/app_colors.dart';
@@ -8,16 +9,20 @@ class LoadingWheel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const LoadingIndicator(
-      indicatorType: Indicator.ballRotateChase,
-      colors: [
-        AppColors.mainColor4,
-        AppColors.mainColor3,
-        AppColors.mainColor2,
-        AppColors.mainColor,
-      ],
-      strokeWidth: 2,
-      backgroundColor: Colors.transparent,
+    return SizedBox(
+      height: 0.2.sh,
+      width:  0.2.sh,
+      child: const LoadingIndicator(
+        indicatorType: Indicator.ballRotateChase,
+        colors: [
+          AppColors.mainColor4,
+          AppColors.mainColor3,
+          AppColors.mainColor2,
+          AppColors.mainColor,
+        ],
+        strokeWidth: 2,
+        backgroundColor: Colors.transparent,
+      ),
     );
   }
 }
